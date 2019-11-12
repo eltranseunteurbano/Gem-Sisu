ef44307162cf5f054b2e5102a921ccedef70309c
Jaime Burbano <burbanojaime98@gmail.com>
2019-11-12 09:17:50 -0500

    home finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/reset.scss';
 
-function App() {
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import * as routes from './constants/Routes';
+
+import Nav from './components/Nav/Nav';
+import Home from './containers/home/Home';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path={routes.HOME}> <Home /> </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/containers/home/Home.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Home.scss';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCommentDots } from '@fortawesome/free-regular-svg-icons'
+import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+    return(
+        <section className="Home">
+            <article className="Home__white">
+                <div className="Home__white__icons">
+                    <Link to="" className="Home__white__icons__icon"><FontAwesomeIcon icon={ faFacebookF }/></Link>
+                    <Link to="" className="Home__white__icons__icon"><FontAwesomeIcon icon={ faInstagram }/></Link>
+                    <Link to="" className="Home__white__icons__icon"><FontAwesomeIcon icon={ faTwitter }/></Link>
+                </div>
+            </article>
+            <article className="Home__yellow">
+                <h1 className="Home__yellow__title">Un gran <strong>texto</strong> aquí <br /> en web</h1>
+            </article>
+
+            <article className="Home__img">
+                <img src={process.env.PUBLIC_URL + '/img/bienvenida.png'} alt="Imagen de Bienvenida"/>
+            </article>
+
+            <article className="Home__chat">
+                <FontAwesomeIcon icon={ faCommentDots } className="Home__chat__icon"/>
+            </article>
+        </section>
+    )
+}
+
+export default Home;
